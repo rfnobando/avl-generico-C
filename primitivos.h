958b73fe861df3170a838808b17f5eb7fe03470d
Rodrigo <rfnobando@gmail.com>
2024-07-22 19:00:55 -0300

    new comments

--- primitivos.h
+++ primitivos.h
@@ -2,16 +2,18 @@
 #define PRIMITIVOS_H_INCLUDED
 
 
+/// Int
+void mostrarInt(int a);
+void mostrarIntVoid(void *a);
 int compararInt(int a, int b);
 int compararIntVoid(void *a, void *b);
 
-int compararString(char a[], char b[]);
-int compararStringVoid(void *a, void *b);
 
-void mostrarInt(int a);
-void mostrarIntVoid(void *a);
+/// String (char *)
 void mostrarString(char a[]);
 void mostrarStringVoid(void *a);
+int compararString(char a[], char b[]);
+int compararStringVoid(void *a, void *b);
 
 
 #endif // PRIMITIVOS_H_INCLUDED
